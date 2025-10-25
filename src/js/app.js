66e923ad92ba0db9ad94109feaaec9121b73e6db
Christopher Sasias <christophersasias@hotmail.com>
2025-10-25 00:23:30 +0000

    Icons ok

--- src/js/app.js
+++ src/js/app.js
@@ -37,10 +37,10 @@ function render(variables = {}) {
             <h2>${variables.role}</h2>
             <h3>${variables.city},${variables.country}</h3>
             <ul class=${variables.SocialMediaPosition}>
-            <li><a href="https://twitter.com/${variables.twitter}"><i class='bx bxl-twitter' ></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class='bx bxl-github'></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class='bx bxl-linkedin'></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class='bx bxl-instagram'></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class='fab fa-twitter' ></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class='fab fa-github'></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class='fab fa-linkedin'></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class='fab fa-instagram'></i></a></li>
         </ul>
           </ul>
         </div>
